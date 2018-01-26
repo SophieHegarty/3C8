89c38f248255eb01d4442c57db0b9c8b98faa483
SophieHegarty <shegarty874@gmail.com>
2018-01-26 15:56:33 +0000

    Replacing signal generator with sketch

---
+++ DigitalDice_clock.ino
@@ -0,0 +1,13 @@
+#define Pin_Clock 12
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(Pin_Clock, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(Pin_Clock, HIGH);
+  delay(10);
+  digitalWrite(Pin_Clock, LOW);
+  delay(10);
+}
