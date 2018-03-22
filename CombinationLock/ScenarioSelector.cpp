8e6aae4ee8be66cb622de4b8ab9628ab3ca1109f
Aron Hoffmann <aron.m.hoff@gmail.com>
2018-03-22 12:36:54 +0000

    Added scenario selector

---
+++ CombinationLock/ScenarioSelector.cpp
@@ -0,0 +1,39 @@
+#include "ScenarioSelector.h"
+
+#include "stl.h"
+
+ScenarioSelector::ScenarioSelector(byte pin0, byte pin1, byte pin2, byte enable_pin)
+        : pins{pin0, pin1, pin2},
+          enable_pin(enable_pin) {
+    for (byte pin : {pin0, pin1, pin2, enable_pin}) {
+        pinMode(pin, INPUT);
+    }
+}
+
+void ScenarioSelector::setScenarioHandler(byte scenario, scenario_handler handler) {
+    handlers[scenario] = handler;
+}
+
+void ScenarioSelector::run() {
+    unsigned long current_time = millis();
+    if (digitalRead(enable_pin)) {
+        if (!enabled && previous_enable_off + debounce_delay < current_time) {
+            enabled = true;
+            previous_enable_on = current_time;
+
+            byte enabled_scenario = 0;
+            for (byte i = 0; i < 2; i++) {
+                enabled_scenario |= (digitalRead(pins[i]) & 1) << i;
+            }
+
+            if (handlers[enabled_scenario] != NULL) {
+                (*handlers[enabled_scenario])();
+            }
+        }
+    } else {
+        if (enabled && previous_enable_on + debounce_delay < current_time) {
+            enabled = false;
+            previous_enable_off = current_time;
+        }
+    }
+}
---
+++ CombinationLock/ScenarioSelector.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <Arduino.h>
+
+class ScenarioSelector {
+public:
+    ScenarioSelector(byte pin0, byte pin1, byte pin2, byte enable_pin);
+
+    void run();
+
+    typedef void(*scenario_handler)();
+    void setScenarioHandler(byte scenario, scenario_handler handler);
+
+private:
+    byte pins[3];
+    byte enable_pin;
+
+    bool enabled = false;
+    unsigned long previous_enable_on = 0;
+    unsigned long previous_enable_off = 0;
+    static constexpr unsigned long debounce_delay = 500;
+
+    scenario_handler handlers[8] = {};
+};
